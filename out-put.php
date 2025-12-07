60f7a55b3a2873a773ff8abb2ade2cb6084f6902
kaedeiwasawa <fyanze93@gmail.com>
2025-12-07 23:38:28 +0900

    ラジオ・セレクト追加

--- out-put.php
+++ out-put.php
@@ -1,18 +1,57 @@
 <?php
 require 'header.php';
 ?>
-<?php
+<!-- <?php
 $price=$_REQUEST['price'];
 $count=$_REQUEST['count'];
 
   echo $price, '円×';
   echo $count, '個=';
   echo ' 料金は、', $price*$count, '円です。<br>';
-
+if ($price != '' || $count != '' ){
+  echo 'いずれかの単価と個数を入力して下さい。';
+}
 if (isset($_REQUEST['mail'])){
   echo 'お買い得情報のメールをお送りさせていただきます。<br>';
 } else {
   echo 'お買い得情報のメールをお送りできません。';
+}
+?> -->
+<?php
+$req=$_REQUEST['meal'];
+if ($req ==  ''){
+  echo 'いずれかのラジオボタンを選択して下さい。<br>';
+}
+switch ($_REQUEST['meal']) {
+case '和食':
+    echo '焼き魚、煮物、味噌汁、ご飯、果物';
+    break;
+case '洋食':
+    echo 'ジュース、ハンバーグ、オムレツ、ハッシュポテト、パン、コーヒー';
+    break;
+case '中華':
+    echo '餃子、チャーハン、ラーメン、春巻き、杏仁豆腐';
+    break;
+case '':
+    echo 'いずれかの料理を選択して下さい。<br>';
+    break;
+}
+echo 'を提供いたします。';
+
+switch ($_REQUEST['seat']) {
+case '指定席':
+    echo '追加料金は1200円です。<br>';
+    break;
+case 'グリーン席':
+    echo '追加料金は2500円です。<br>';
+    break;
+default:
+    echo '追加料金はありません。<br>';
+    break;
+}
+
+for ($i=0; $i<10; $i++){
+    echo '<option value="',$i, '">', $i, '</option>';
 }
 ?>
 <?php require 'footer.php';?>
--- test.php
+++ test.php
@@ -2,14 +2,33 @@
 require './header.php';
 const MESSAGE='単価と個数を入力して下さい。';
 echo MESSAGE;
-$price='';
 ?>
 <!-- <p>お名前を入力してください。</p> -->
 <form action="out-put.php" method="post">
-  単価<input type="text" name="price"> 円
+  <!-- 単価<input type="text" name="price" value="0"> 円
   ×
-  個数<input type="text" name="count"> 円<br>
-  <input type="checkbox" name="mail"><br>
-  <input type="submit" value="確定">
+  個数<input type="text" name="count" value="0">円<br>
+  <input type="checkbox" name="mail"><br> -->
+  <p><input type="radio" name="meal" value="和食">和食</p>
+  <p><input type="radio" name="meal" value="洋食">洋食</p>
+  <p><input type="radio" name="meal" value="中華">中華</p>
+  <select name="seat">
+    <option value="">席を指定して下さい。</option>
+    <option value="自由席">自由席</option>
+    <option value="指定席">指定席</option>
+    <option value="グリーン席">グリーン席</option>
+  </select>
+  <select name="num">
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    <option value="9">9</option>
+  </select>
+  <p><input type="submit" value="確定"></p>
 </form>
 <?php require './footer.php' ?>
